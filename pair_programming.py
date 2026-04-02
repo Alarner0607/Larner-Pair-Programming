9891c9726805300c5cd65d53ebfc17e0eb8b0edb
Nina House <nina_house@mines.edu>
2026-04-01 18:59:27 -0600

    Added test function and feedback

--- pair_programming.py
+++ pair_programming.py
@@ -6,7 +6,11 @@ def conversion():
     meters = (0.3048 * feet) + (0.0254 * inches)   #Conversion math
     print(f"{meters} Meters")   #Statement
 
-#Enter values as only numbers
+#Enter values as only numbers 
 #Run the function as just conversion()
 
 conversion()
+
+#Partner Notes: The code generally works as intended in normal cases however the converstion 
+# of inputed values to integers may cause minor errors if the inputed number was not an integer 
+# to begin with. The code does properly handle negative integers and the number 0.
